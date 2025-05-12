b878430370456e5448b8b9789198f6854f6b0e22
jily64 <108062664+jily64@users.noreply.github.com>
2025-05-12 17:22:58 +0300

    Linux StartUp

---
+++ install.sh
@@ -0,0 +1,4 @@
+python3 -m venv .venv
+. .venv/bin/activate
+pip install pymavlink kivy python_dotenv
+deactivate
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import kivy
-from kivy.core.window import Window
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.animation import Animation
+from kivy.uix.label import Label
 from kivy.uix.image import Image
-from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Rectangle, Color, Rotate, PushMatrix, PopMatrix
+from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.graphics.svg import Svg
+from kivy.animation import Animation
+from kivy.lang import Builder
+from kivy.properties import NumericProperty
 
-from Modules import Adapter
+import random
 
-PATH = "." + "/resources/"
+from Modules import Adapter as MAVLinkAdapter
 
-import os
-os.environ['KIVY_INPUT'] = 'mouse,multitouch_on_demand'
 
-class Horizon(Image):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class Sprite( Image ):
+    angle = NumericProperty(0)
+
+    def __init__( self, x=0, y=0, **kwargs ):
+        super( Sprite, self ).__init__( **kwargs )
+        
+        self.angle_to = 0
 
         with self.canvas.before:
             PushMatrix()
@@ -29,58 +31,79 @@ class Horizon(Image):
             self.rot.angle  = 0
             self.rot.origin = self.center
             self.rot.axis = (0, 0, 1)
-
+        
+        self.allow_stretch = True
         with self.canvas.after:
             PopMatrix()
-
-        self.source = PATH + "Background.png"
-        self.reload()
-
-        self.texture_size = 1920*2.5, 1080*2
-        self.fit_mode = "contain"
-        self.size = 1920*2.5, 1080*2
-
-        self.center = 1920//2, 1080//2
-
+        
         self.angle = 0
+        self.source = 'resources/Background.png'
+        self.size = (1920*2, 1080*2)
+        self.pos = (0, 0)
+        self.fit_mode = "contain"
+
+        self.center = (1970/2, 1100/2)
+
         self.rot.origin = self.center
+        self.animate()
 
-        #self.animate()
+    def animateComplete( self, *kargs ):
+        Animation.cancel_all( self )
+        self.rot.angle = self.angle_to
+        self.animate()
 
-    def animate(self):
-        self.anim = Animation(angle=self.angle, duration=0)
-        self.anim.bind(on_complete=self.update_anim)
-        self.anim.repeat = False
-        self.anim.start(self.rot)
+    def animate( self ):
+        self.anim = Animation( angle=self.angle_to, duration=0.05 )
+        self.anim.bind( on_complete=self.animateComplete )
+        self.anim.repeat = True
+        self.anim.start( self.rot ) 
 
-    def update_anim(self, *kargs):
-        #self.animate()
-        pass
+class AltLabel(Label):
+    def __init__(self, **kwargs):
+        super(Label, self).__init__(**kwargs)
+
+        self.text = "123"
+        self.font_style='H1'
+        self.center = (1920//2, 1080//2)
 
 
-class SimpleApp(App):
+
+class Applet(App):
     def build(self):
+        self.mav = MAVLinkAdapter.Adapter("udp:0.0.0.0:14550")
+        self.c = 0
+
         Window.fullscreen = 'auto'
-        self.root = Widget()
 
-        self.mav = Adapter.Adapter("udp:0.0.0.0:14550")
+        root = Widget()
 
-        self.horizon = Horizon()
+        self.img = Image(source ='resources/Background.png')
 
-        self.root.add_widget(self.horizon)
+        Clock.schedule_interval(self.update, 1 / 150)
 
-        Clock.schedule_interval(self.update_mav, 0)
+        self.spr = Sprite()
+        self.altLabel = Label(text='1000 М.')
+        self.altLabel.center = (175, 1080//2)
+        self.altLabel.font_size = 75
 
-        return self.root
+        root.add_widget(self.spr)
+        root.add_widget(self.altLabel)
+
+        return root
     
-    def update_mav(self, dt):
+    def update(self, dt):
         self.mav.update()
-        if self.mav.data["heading"] != None:
-            self.horizon.rot.angle = self.mav.data["attitude"]["roll"]
-        print(self.mav.data)
+        if self.mav.data["attitude"]["roll"] != None:
+            self.spr.angle_to = self.mav.data["attitude"]["roll"]
+            self.spr.rot.origin = (1920/2, 1080/2)
 
-    def ok2(self, dt):
-        pass
+            self.altLabel.text = str(self.mav.data["global_position"]["alt"]) + " М."
+        else:
+            self.spr.angle_to = 0
+
+
+
+
 
 if __name__ == '__main__':
-    SimpleApp().run()
+    Applet().run()
